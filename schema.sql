7829b7330f5c974eaa6b29b3199e45995ac5e3b2
Marcus Harelstad <mharelstad2@student.cvtc.edu>
2023-11-06 18:31:08 -0600

    Fixed the schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
--- Create the "buttons" table to track behaviors
+-- Create buttons table
 CREATE TABLE buttons (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL
 );
 
--- Create the "log" table to track button clicks
+-- Create log table
 CREATE TABLE log (
-  button_id INTEGER REFERENCES buttons(id) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  button_id INTEGER NOT NULL,
   at TIMESTAMPTZ NOT NULL
 );
-
 
 -- Insert a sample button
 INSERT INTO buttons (name) VALUES ('Test');
